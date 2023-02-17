59930463e3904eeabefab7c038ff3a3efb6f29a8
Hilton W. Silva <me@hilton.ws>
2023-02-17 16:11:51 +0000

    Purge CSS fix

--- docs/main.js
+++ docs/main.js
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
   \************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/scss/styles.scss\");\n// Import our custom CSS\n\n\n//# sourceURL=webpack://hiltonws.github.io/./src/js/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/scss/styles.scss\");\n// Import our custom CSS\r\n\n\n//# sourceURL=webpack://hiltonws.github.io/./src/js/main.js?");
 
 /***/ })
 
